a299931f9c4bc61e83d0c322ce2ee4b0c41ddc48
thangchuot992011 <47500772+thangchuot992011@users.noreply.github.com>
2021-11-22 23:57:41 +0700

    Thêm hàm load dữ liệu cho Trang chủ
    Hàm load dữ liệu cho Trang chủ từ file trealet riêng của đơn vị

--- lib/customFunc.php
+++ lib/customFunc.php
@@ -8,4 +8,26 @@ function getItemsFromJSON($filePath) {
 	return $d['listItems'];
 }
 
+function loadCustomPageInfo($filePath) {
+	$json_string = file_get_contents($filePath);	
+	if(!$json_string) die("Không lấy được dữ liệu!");
+	$d = json_decode($json_string, true);	
+	if(!$d) die('Cannot parse the trealet content!');	
+	if(!isset($d['trealet'])) die('It is not a trealet!');	
+	if($d['trealet']['exec']!='streamline') die('Wrong executable name!');
+	return $d['trealet'];
+}
+
+function localFetchItemData($item_url){
+	if(is_numeric($item_url)){
+		$item_url = 'https://hcloud.trealet.com/tiny'.$item_url.'/?json';
+		$json_string = file_get_contents($item_url);
+		if(!$json_string) return;	
+		$d = json_decode($json_string, true);
+		return $d['image'];
+	}
+	else {
+		return $item_url;
+	}
+}
 ?>
