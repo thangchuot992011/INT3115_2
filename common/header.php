96c77ce8f32d733b6d91b178561d77d109806b5c
thangchuot992011 <47500772+thangchuot992011@users.noreply.github.com>
2021-11-22 23:59:32 +0700

    Sửa lại link trên Header
    Sửa lại link trên Header hiển thị theo đơn vị

--- common/header.php
+++ common/header.php
@@ -1,11 +1,27 @@
+<?php
+    $pageStr = '';
+    if(isset($_GET['page'])) {
+        $pageStr = '?page=' . $_GET['page'];
+    } else {
+        if(isset($_GET['trealet'])) {
+            $trealetLink = $_GET['trealet'];
+            $pageStr = str_replace('albums/nhom01/app/streamline/','', $trealetLink);
+            $pageStr = str_replace('/', '@', $pageStr);
+            $pageStr= preg_replace('/@.*/i', '', $pageStr);
+            if($pageStr != '') {
+                $pageStr = '?page=' . $pageStr;
+            }
+        }
+    }
+?>
 <header class="ctn-header">
     <div class="container ctn-main-menu">
         <div class="main-menu">
             <nav class="main-nav">
                 <ul>
-                    <li><a href="<?php echo $GLOBALS['mainDir'] . '/index.php';?>">Trang chủ</a></li>
+                    <li><a href="<?php echo $GLOBALS['mainDir'] . '/index.php'. $pageStr;?>">Trang chủ</a></li>
                     <li><a href="#">Giới thiệu</a></li>
-                    <li><a href="<?php echo $GLOBALS['mainDir'] . '/items.php';?>">Hiện vật</a></li>
+                    <li><a href="<?php echo $GLOBALS['mainDir'] . '/items.php' . $pageStr;?>">Hiện vật</a></li>
                 </ul>
             </nav>
         </div>
