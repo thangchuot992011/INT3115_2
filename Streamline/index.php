6be6d17dd736d5d2e296a34e0f6888bbe4cc3ac4
Duong Dinh <duongdinh160799@gmail.com>
2021-11-24 20:27:17 +0700

    fix css navbar

--- Streamline/index.php
+++ Streamline/index.php
@@ -38,114 +38,120 @@
     <div class="main-container">
         <?php require_once($commonFolder . "/header.php");?>
         <!--Welcome-->
-        <section id="button-link-web">
-            <div class="container">
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                    <a class="navbar-brand" href="#">Vertical Navbar</a>
-                    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
+        <div class="row">
+            <div class="col-1">
+                <section id="button-link-web">
+                    <div class="container">
+                        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                            <a class="navbar-brand" href="#">Vertical Navbar</a>
+                            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
 
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav mr-auto flex-column vertical-nav">
-                            <li class="nav-item">
-                                <a class="nav-link" href="#welcome">Giới thiệu</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#image">Hình Ảnh</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#video">Video</a>
-                            </li>
-                        </ul>
-                    </div>
-                </nav>
+                            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                                <ul class="navbar-nav mr-auto flex-column vertical-nav edit-nav">
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#welcome">Giới thiệu</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#image">Hình Ảnh</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" href="#video">Video</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </nav>
 
+                    </div>
+                </section>
             </div>
-        </section>
-        <section id="welcome" class="padding">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <h2 class="heading"><?php echo $trealetData["title"];?></h2>
-                        <hr class="heading_space">
-                    </div>
-                    <div class="col-md-7 col-sm-6">
-                        <?php foreach($info as $infoKey => $infoValue) { ?>
-                            <p class="half_space"><strong><?php echo $infoKey;?></strong>: <?php echo $infoValue;?></p>
-                        <?php } ?>
-                        <p class="half_space"></p>
-                        <p class="half_space"><?php echo $trealetData["decs"];?></p>
-                    </div>
-                    <div class="col-md-5 col-sm-6">
-                        <img class="img-responsive" src="<?php echo $trealetData["titleImage"] ;?>" alt="image">
-                    </div>
-                </div>
-            </div>
-        </section>
-        <!--Food Facilities-->
-        <section id="image" class="padding bg_grey">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <h2 class="heading">Hình Ảnh</h2>
-                        <hr class="heading_space">
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col-md-12 grid_layout">
+            <div class="col-11">
+                <section id="welcome" class="padding">
+                    <div class="container">
                         <div class="row">
-                            <div class="zerogrid">
-                                <div class="wrap-container">
-                                    <div class="wrap-content clearfix">
-                                        <?php if($numOfImageItems > 0) { ?>
-                                            <div class="col-3-5 bachground-color-image d-flex align-items-center ">
-                                                <div class="wrap-col first">
-                                                    <div class="item-container">
-                                                        <img class="image-fix-size" src="<?php echo $imageItems[0]; ?>" alt="cook"/>
+                            <div class="col-md-12">
+                                <h2 class="heading"><?php echo $trealetData["title"];?></h2>
+                                <hr class="heading_space">
+                            </div>
+                            <div class="col-md-7 col-sm-6">
+                                <?php foreach($info as $infoKey => $infoValue) { ?>
+                                    <p class="half_space"><strong><?php echo $infoKey;?></strong>: <?php echo $infoValue;?></p>
+                                <?php } ?>
+                                <p class="half_space"></p>
+                                <p class="half_space"><?php echo $trealetData["decs"];?></p>
+                            </div>
+                            <div class="col-md-5 col-sm-6">
+                                <img class="img-responsive" src="<?php echo $trealetData["titleImage"] ;?>" alt="image">
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <!--Food Facilities-->
+                <section id="image" class="padding bg_grey">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h2 class="heading">Hình Ảnh</h2>
+                                <hr class="heading_space">
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-md-12 grid_layout">
+                                <div class="row">
+                                    <div class="zerogrid">
+                                        <div class="wrap-container">
+                                            <div class="wrap-content clearfix">
+                                                <?php if($numOfImageItems > 0) { ?>
+                                                    <div class="col-3-5 bachground-color-image d-flex align-items-center ">
+                                                        <div class="wrap-col first">
+                                                            <div class="item-container">
+                                                                <img class="image-fix-size" src="<?php echo $imageItems[0]; ?>" alt="cook"/>
+                                                            </div>
+                                                        </div>
                                                     </div>
-                                                </div>
-                                            </div>
-                                        <?php } ?>
-                                        <?php for($i = 1; $i < $numOfImageItems; $i++) { ?>
-                                            <div class="col-1-5 bachground-color-image">
-                                                <div class="wrap-col first ">
-                                                    <div class="item-container">
-                                                        <img class="image-fix-size" src="<?php echo $imageItems[$i]; ?>" alt="cook"/>
+                                                <?php } ?>
+                                                <?php for($i = 1; $i < $numOfImageItems; $i++) { ?>
+                                                    <div class="col-1-5 bachground-color-image">
+                                                        <div class="wrap-col first ">
+                                                            <div class="item-container">
+                                                                <img class="image-fix-size" src="<?php echo $imageItems[$i]; ?>" alt="cook"/>
+                                                            </div>
+                                                        </div>
                                                     </div>
-                                                </div>
+                                                <?php } ?>
                                             </div>
-                                        <?php } ?>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
-        </section>
+                </section>
 
-        <!--Featured Receipes -->
-        <section id="video" class="bg_grey padding">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <h2 class="heading">Video</h2>
-                        <hr class="heading_space">
-                    </div>
-                </div>
-                <div class="row">
-                    <?php 
-                        $numOfVideos = count($trealetData["videos"]);
-                        for($i = 0; $i < $numOfVideos; $i++) {
-                    ?>
-                        <div class="col-1-2 bachground-color-image">
-                            <iframe width="560" height="315" src="<?php echo $trealetData["videos"][$i]; ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <!--Featured Receipes -->
+                <section id="video" class="bg_grey padding">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12">
+                                <h2 class="heading">Video</h2>
+                                <hr class="heading_space">
+                            </div>
                         </div>
-                    <?php } ?>
-                </div>
+                        <div class="row">
+                            <?php
+                            $numOfVideos = count($trealetData["videos"]);
+                            for($i = 0; $i < $numOfVideos; $i++) {
+                                ?>
+                                <div class="col-1-2 bachground-color-image">
+                                    <iframe width="560" height="315" src="<?php echo $trealetData["videos"][$i]; ?>" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                                </div>
+                            <?php } ?>
+                        </div>
+                    </div>
+                </section>
             </div>
-        </section>
+        </div>
         <?php require_once($commonFolder . "/footer.php");?>
     </div>
     <?php require_once($commonFolder . "/scripts.php");?>
