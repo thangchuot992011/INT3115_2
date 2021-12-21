7f7426d09ede08374d06c9611103ba394f1d975e
thangchuot992011 <47500772+thangchuot992011@users.noreply.github.com>
2021-12-21 21:22:36 +0700

    Sửa lại các hàm load dữ liệu

--- lib/customFunc.php
+++ lib/customFunc.php
@@ -1,20 +1,30 @@
 <?php
+
+function customInitializeApp($mainDir, $exec, &$listItems){
+	$filePath = $mainDir . $_GET['trealet'];
+	return loadCustomPageInfo($filePath, $listItems);
+}
+
 function getItemsFromJSON($filePath) {
 	$json_string = file_get_contents($filePath);	
 	if(!$json_string) die("Không lấy được dữ liệu!");
 	$d = json_decode($json_string, true);	
-	if(!$d) die('Không lấy được dữ liệu!');	
+	if(!$d) die('Cannot parse the trealet content!');
 	if(!isset($d['listItems'])) die('Không lấy được dữ liệu!');	
+	if(!isset($d['trealet'])) die('It is not a trealet!');	
+	if($d['trealet']['exec']!='streamline') die('Wrong executable name!');
 	return $d['listItems'];
 }
 
-function loadCustomPageInfo($filePath) {
-	$json_string = file_get_contents($filePath);	
+function loadCustomPageInfo($filePath, &$listItems) {
+	$json_string = file_get_contents($filePath);
 	if(!$json_string) die("Không lấy được dữ liệu!");
 	$d = json_decode($json_string, true);	
 	if(!$d) die('Cannot parse the trealet content!');	
 	if(!isset($d['trealet'])) die('It is not a trealet!');	
 	if($d['trealet']['exec']!='streamline') die('Wrong executable name!');
+	if(!isset($d['listItems'])) die('Không thể lấy được dữ liệu!');
+	$listItems = $d['listItems'];
 	return $d['trealet'];
 }
 
